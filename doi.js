9e384060c99d7fb8bdcc7d0580c61289869b6e03
Harsh Rajput <32320131+gradeb@users.noreply.github.com>
2019-07-19 16:04:27 +0530

    Update doi.js

--- doi.js
+++ doi.js
@@ -4,19 +4,21 @@ const createStyledDiv = ( a = {} ) => addStyle(a, document.createElement("div"))
 
 const createProgressBar = ( bgColor = "#ccc", fgColor = "#000", height = "10px", transition = "0ms" ) => {
 
-  const b = createStyledDiv({ background: bgColor, position: "fixed", top: 0, left: 0, width: "100%", zIndex: 9999 });
+  const b = createStyledDiv({ background: bgColor, position: "sticky", top: 0, left: 0, width: "100%", zIndex: 9999 });
 
   const c = createStyledDiv({ background: fgColor, height, width: 0, transition });
 
+  const e = document.body;
+
   b.appendChild( c );
 
-  document.body.appendChild( b );
+  e.insertBefore( b, e.firstElementChild );
 
   return {
 
     move: d => c.style.width = (d <= 100 ? d : 100) + "%",
 
-    hide: () => b.style.opacity = 0
+    hide: () => b.style.display = "none"
 
   };
 
